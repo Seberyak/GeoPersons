234caef4ec2906aac7f27f9c1ac24cfac27c2fd8
seberyak <v_liparteliani@cu.edu.ge>
2020-07-02 15:32:56 +0400

    api cors bug fixed

--- GeoPerson/settings.py
+++ GeoPerson/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django
-# import django_heroku
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -135,4 +134,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
